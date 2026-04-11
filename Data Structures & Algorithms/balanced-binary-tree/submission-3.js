7d1693de8d0d842a1070ceb7f4c20f6bec939f4a
Johnkm09 <johnkm09@gmail.com>
2026-04-11 14:44:42 +0300

    Add: balanced-binary-tree - submission-3

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-3.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        function depth(node){
+            if(node === null) return 0;
+            let left = depth(node.left);
+            if(left === -1) return -1;
+
+            let right = depth(node.right);
+            if(right === -1) return -1;
+
+            if(Math.abs(left - right) > 1) return -1;
+
+            return Math.max(left,right) + 1;
+        }
+        return depth(root) !== -1;
+    }
+}
