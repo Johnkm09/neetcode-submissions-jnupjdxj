2b96f0f2485a607f66e41c5cbe2480628888cec9
Johnkm09 <johnkm09@gmail.com>
2026-05-05 12:45:07 +0300

    Add: meeting-schedule - submission-15

---
+++ Data Structures & Algorithms/meeting-schedule/submission-15.js
@@ -0,0 +1,32 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+        const n = intervals.length;
+        if(n <= 1) return true;
+
+        intervals.sort((a,b) => a.start - b.start);
+
+        for(let i = 1; i < n; i++){
+            const curr = intervals[i];
+            const prev = intervals[i - 1];
+
+            if(curr.start < prev.end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
