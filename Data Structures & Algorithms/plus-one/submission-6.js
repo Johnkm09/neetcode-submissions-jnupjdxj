8b14ae07a7a0c993cee12adc8cc4b083ebceb65c
Johnkm09 <johnkm09@gmail.com>
2026-05-05 18:18:06 +0300

    Add: plus-one - submission-6

---
+++ Data Structures & Algorithms/plus-one/submission-6.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} digits
+     * @return {number[]}
+     */
+    plusOne(digits) {
+        let n = digits.length;
+
+        for(let i = n - 1; i >= 0; i--){
+            //19
+            if(digits[i] < 9){
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits.unshift(1);
+        return digits;
+    }
+}
