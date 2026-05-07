fc2fda8142bb950d62d440ed45329c13a2ddf726
Johnkm09 <johnkm09@gmail.com>
2026-05-07 12:33:12 +0300

    Add: single-number - submission-5

---
+++ Data Structures & Algorithms/single-number/submission-5.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums) {
+        let mp = {};
+
+        for(let i = 0; i < nums.length; i++){
+            if(mp[nums[i]] === undefined){
+                mp[nums[i]] = 1;
+            }else{
+                mp[nums[i]] += 1;
+            }
+        }
+
+        for(let key in mp){
+            if(mp[key] === 1){
+                return Number(key);
+            }
+        }
+    }
+}
