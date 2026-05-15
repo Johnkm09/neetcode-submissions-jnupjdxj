2e26c9db26b553a31847c06523c1f8c66783178c
Johnkm09 <johnkm09@gmail.com>
2026-05-15 18:56:01 +0300

    Add: is-palindrome - submission-27

---
+++ Data Structures & Algorithms/is-palindrome/submission-27.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let start = 0;
+        let end = s.length - 1;
+
+        while(start < end){
+            while(start < end && !/[a-z0-9]/i.test(s[start])) start++;
+            while(start < end && !/[a-z0-9]/i.test(s[end])) end--;
+            if(s[start].toLowerCase() === s[end].toLowerCase()){
+                start++;
+                end--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
