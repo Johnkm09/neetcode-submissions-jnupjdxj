497431d5d7e62269bf24612ad792fb4403121bbd
Johnkm09 <johnkm09@gmail.com>
2026-06-21 19:56:28 +0300

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    isAnagram(i, j){
+        if(i.length !== j.length) return false;
+
+        return i.split('').sort().join('') === j.split('').sort().join('');
+    }
+    groupAnagrams(strs) {
+        const visited = new Array(strs.length).fill(false);
+        const result = [];
+
+        for(let i = 0; i < strs.length; i++){
+            if(visited[i]) continue;
+
+            const group = [strs[i]];
+            visited[i] = true;
+
+            for(let j = i + 1; j < strs.length; j++){
+                if((!visited[j]) && this.isAnagram(strs[i],strs[j])){
+                    group.push(strs[j]);
+                    visited[j] = true;
+                }
+            }
+            result.push(group);
+        }
+        return result;
+    }
+}
